d4e2f1079f427b21ff298d56a74de61205c83def
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-04-03 13:15:20 -0700

    Add header nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <h1>Kala Yamaguchi</h1>
+    <>
+      <header>
+        <h1>Kala Yamaguchi</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">home</Link>
+            </li>
+            <li>
+              <Link href="/about">about</Link>
+            </li>
+            <li>
+              <Link href="/resume">resume</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   )
 }
