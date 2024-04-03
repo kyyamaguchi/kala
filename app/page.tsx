172620927a5058d01150e5e540b812381ca85927
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-04-03 13:30:14 -0700

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import styles from './Home.module.css'
 
 export default function Home() {
   return (
@@ -19,6 +21,16 @@ export default function Home() {
           </ul>
         </nav>
       </header>
+      <section id={styles.hero}>
+        <Image
+          unoptimized
+          src='/kala-illustration.png'
+          alt='An illustration of Kala Yamaguchi'
+          width={734}
+          height={949}
+        />
+        <p>A designer, artist, human being, and avid practitioner of the K.I.S.S. philosophy.</p>
+      </section>
     </>
   )
 }
