f708ac49cf77b9318baad111763685efb867b266
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-07-02 19:08:52 -0700

    Update hero to match Figma

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,7 @@ export default function Home() {
           width={734}
           height={949}
         />
-        <div>
-          <p><strong>Kala Yamaguchi</strong> is a native Hawaiian designer, illustrator, human being, and an avid practitioner of the K.I.S.S. philosophy.</p>
-          <p>Currently, he&apos;s based out of San Francisco, CA.</p>
-        </div>
+        <p><strong>Kala Yamaguchi</strong> is a Native Hawaiian designer & illustrator. Based out of San Francisco, CA.</p>
       </section>
       <GalleryCard
         src="/thumbnail-2.png"
