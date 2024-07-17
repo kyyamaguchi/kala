6553ef8af61d5ef337244a4144b6ee49b0eeae60
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-07-16 17:53:51 -0700

    Optimize `GalleryCard` and add support for video

--- components/GalleryCard/GalleryCard.tsx
+++ components/GalleryCard/GalleryCard.tsx
@@ -1,61 +1,80 @@
 import Link from 'next/link'
-import { Route } from 'next'
-import Image from 'next/image'
+import { type Route } from 'next'
+import fs from 'node:fs/promises'
+import { getPlaiceholder } from 'plaiceholder'
+import Image, { type ImageProps } from 'next/image'
 
 import { Spacer } from '@/components/Spacer'
 
 import styles from './GalleryCard.module.css'
 
-export function GalleryCard(
-  {
-    title,
-    description,
-    link,
-    cta,
-    src,
-    alt,
-    maxWidth
-  }:
-  {
-    title?: string,
-    description?: React.ReactElement,
-    link?: Route,
-    cta?: string,
-    src: string,
-    alt?: string,
-    maxWidth?: number,
-  }
+export interface GalleryCardProps extends Omit<ImageProps, 'alt'> {
+  title?: string
+  description?: React.ReactElement
+  link?: Route
+  cta?: string
+  alt?: string
+  video?: boolean
+}
+
+export async function GalleryCard(
+  { title, description, link, cta, alt, video, ...props }:
+  GalleryCardProps
 ) {
-  const image = (
-    <Image
-      unoptimized
-      src={src}
-      alt={alt || title || ''}
-      fill={true}
-    />
+  async function processImage(src: string) {
+    const file = await fs.readFile(src)
+    const { base64, metadata } = await getPlaiceholder(file)
+    return { base64, metadata }
+  }
+  let imageData
+  if (!video) imageData = await processImage(`public${props.src}`)
+  const isGif = props.src.toString().endsWith('.gif')
+
+  const ImageWrapper = (
+    <>
+      { !video &&
+        <Image
+          unoptimized={isGif}
+          alt={alt || title || ''}
+          placeholder={`${isGif ? 'empty' : 'blur'}`}
+          blurDataURL={imageData?.base64}
+          width={imageData?.metadata.width}
+          height={imageData?.metadata.height}
+          {...props}
+        />
+      }
+    </>
   )
+
+  const VideoWrapper = (
+    <>
+      {
+        video &&
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+          >
+            <source src={props.src as string} type="video/mp4" />
+          </video>
+      }
+    </>
+  )
+
   return (
     <>
-      <div
-        className={styles["image-card"]}
-        style={{ maxWidth: maxWidth || '1400px' }}
-      >
+      <div className={styles.card}>
         <figure>
-          { link ? <Link href={link}>{image}</Link> : image }
-          {
-            (title && !link)
-              && <figcaption>{title}</figcaption>
-          }
-          {
-            (title && link)
-              && <Link href={link}><figcaption>{title}</figcaption></Link>
-          }
-          { description && <div>{description}</div> }
+          {link ? <Link href={link}>{ImageWrapper}</Link> : ImageWrapper}
+          {link ? <Link href={link}>{VideoWrapper}</Link> : VideoWrapper}
+          {title && <figcaption>{title}</figcaption>}
+          {description && <div>{description}</div>}
           {
             link &&
               <>
                 <Spacer size={10} />
-                <p style={{ color: "black" }}><Link href={link}>{cta}</Link></p>
+                <Link href={link}>{cta}</Link>
               </>
             }
         </figure>
