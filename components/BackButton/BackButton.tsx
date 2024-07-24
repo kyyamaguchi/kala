166f6efa2340a15c5bdfc293157aa45462356213
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-07-24 12:36:37 -0700

    Increase back & top button stroke width, contrast

--- components/BackButton/BackButton.tsx
+++ components/BackButton/BackButton.tsx
@@ -7,7 +7,7 @@ export function BackButton() {
     <div aria-label="Back to home page">
       <Link href={`/`} id={styles.button}>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M10 5.75L3.75 12L10 18.25M4.5 12H20.25" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M10 5.75L3.75 12L10 18.25M4.5 12H20.25" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
       </Link>
     </div>
--- components/TopButton/TopButton.tsx
+++ components/TopButton/TopButton.tsx
@@ -37,7 +37,7 @@ export function TopButton() {
         disabled={!show}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-          <path d="M5.75 10L12 3.75L18.25 10M12 20.25V4.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+          <path d="M5.75 10L12 3.75L18.25 10M12 20.25V4.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
       </button>
     </div>
