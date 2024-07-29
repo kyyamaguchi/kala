5e07beae0805bd27c0ce843f82ae58149b2769fb
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-07-29 13:07:57 -0700

    Complete "below the fold" styles

--- components/BelowTheFold/BelowTheFold.tsx
+++ components/BelowTheFold/BelowTheFold.tsx
@@ -11,14 +11,16 @@ export function BelowTheFold(
 ) {
   return (
     <>
-      <Link href={link}>
-        <header className={styles.header}>
+      <hr id={styles.divider} />
+      <Link href={link} id={styles["link-wrapper"]}>
+      <p>Next project</p>
+        <header id={styles.header}>
           <Grid columns={2}>
             <div>
-              <h1 className={styles.title}>{title}</h1>
-              <p className={styles.subtitle}>{area}, {season}</p>
+              <h1 id={styles.title}>{title}</h1>
+              <p id={styles.subtitle}>{area}, {season}</p>
             </div>
-            <p className={styles.description}>{description}</p>
+            <p id={styles.description}>{description}</p>
           </Grid>
         </header>
       </Link>
