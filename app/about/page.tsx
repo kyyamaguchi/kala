bb8d4a51c8837396389237af7c890ff0c28002e6
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-11-08 15:12:50 -0800

    Add About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,35 @@
+import Link from "next/link"
+import Image from "next/image"
+import { Spacer } from "@/components/Spacer"
+
+import styles from "./About.module.css"
+
 export default function About() {
   return (
     <>
-      <h1>About</h1>
+      <Spacer size={64} />
+      <article className={styles.about}>
+        <Image
+          src="/headshot.png"
+          alt="A headshot of Kala Yamaguchi"
+          width={310}
+          height={417}
+        />
+        <section className={styles.text}>
+          <p>
+            Hey, I&apos;m Kala. I&apos;m a designer who&apos;s truly cool and has a fun time. Originally from Oahu, Hawai&apos;i, I moved to San Francisco for undergrad, where I&apos;m currently in my 4th year. I grew up playing baseball (...)
+          </p>
+          <p>
+            You can learn more about my work on my <Link href="#">résumé</Link>.
+          </p>
+          <p>
+            You can connect with me on <Link href="https://linkedin.com/in/kalayamaguchi">LinkedIn</Link>, or drop me a note through <Link href="#">email</Link>.
+          </p>
+          <p>
+            I&apos;m always looking to expand my collection of design books — send me a rec below!
+          </p>
+        </section>
+      </article>
     </>
   )
 }
