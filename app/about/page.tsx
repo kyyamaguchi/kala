8ee2e3b6d901953af522ed6bdf6f99213a42e8ca
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-11-21 23:05:27 -0800

    Increase About page spacing

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -17,16 +17,16 @@ export default function About() {
         />
         <section className={styles.text}>
           <p>
-            Hey, I&apos;m Kala. I&apos;m a designer who&apos;s truly cool and has a fun time. Originally from Oahu, Hawai&apos;i, I moved to San Francisco for undergrad, where I&apos;m currently in my 4th year. I grew up playing baseball (...)
+            <strong>Hey, I&apos;m Kala.</strong>
+          </p>
+          <p>
+            I&apos;m a designer who&apos;s truly cool and has a fun time. Originally from Oahu, Hawai&apos;i, I moved to San Francisco for undergrad, where I&apos;m currently in my 4th year. I grew up playing baseball (...)
           </p>
           <p>
             You can learn more about my work on my <Link href="#">résumé</Link>.
           </p>
           <p>
-            You can connect with me on <Link href="https://linkedin.com/in/kalayamaguchi">LinkedIn</Link>, or drop me a note through <Link href="#">email</Link>.
-          </p>
-          <p>
-            I&apos;m always looking to expand my collection of design books — send me a rec below!
+            You can connect with me on <Link href="https://linkedin.com/in/kalayamaguchi">LinkedIn</Link>, or drop me a note via <Link href="#">email</Link>.
           </p>
         </section>
       </article>
