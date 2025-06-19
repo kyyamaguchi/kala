943338c1874f17fd8d521925a3080ab2def8308f
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-06-19 14:28:10 -0700

    Update Footer styles, add icons

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-
-import { Grid } from '@/components/Grid'
+import { IconLink } from '@/components/Icon'
 import { Spacer } from '@/components/Spacer'
-import { NavItems } from '@/components/NavItems'
+
+import { usePathname } from 'next/navigation'
 
 import styles from './Footer.module.css'
 
@@ -16,31 +15,24 @@ export function Footer() {
     <>
       <Spacer size={`10vh`} />
       <footer id={styles.footer}>
-        <Grid columns={3} columnSizeDistribution={['3fr', '1fr', '1fr']}>
-          <p>Kala Yamaguchi</p>
-          <nav id={styles["site-links"]}>
-            <NavItems />
-          </nav>
-          <nav id={styles["external-links"]}>
-            <ul>
-              <li>
-                <Link href="/">
-                  Résumé
-                </Link>
-              </li>
-              <li>
-                <Link href="/">
-                  Email
-                </Link>
-              </li>
-              <li>
-                <Link href="https://linkedin.com/in/kalayamaguchi">
-                  LinkedIn
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </Grid>
+        <p>© 2025</p>
+        <p><Link href="mailto:kalayamaguchi@gmail.com">kalayamaguchi@gmail.com</Link></p>
+        <nav id={styles["external-links"]}>
+          <ul>
+            <li>
+              <IconLink
+                type="LinkedIn"
+                href="https://linkedin.com/in/kalayamaguchi"
+              />
+            </li>
+            <li>
+              <IconLink
+                type="Instagram"
+                href="https://instagram.com/kalayamaguchi"
+              />
+            </li>
+          </ul>
+        </nav>
       </footer>
     </>
   )
---
+++ components/Icon/index.ts
@@ -0,0 +1,1 @@
+export { Icon, IconLink } from './Icon'
