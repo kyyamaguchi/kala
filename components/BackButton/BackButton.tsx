1d74a976a5c621f4bc0d6dcd596d7c3ef066189d
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-07-18 11:16:53 -0700

    Sharpen corners on chevron

--- components/BackButton/BackButton.tsx
+++ components/BackButton/BackButton.tsx
@@ -7,7 +7,7 @@ export function BackButton() {
     <div aria-label="Back to home page">
       <Link href={`/`} id={styles.button}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M15.7071 3.29289C16.0976 3.68342 16.0976 4.31658 15.7071 4.70711L8.41421 12L15.7071 19.2929C16.0976 19.6834 16.0976 20.3166 15.7071 20.7071C15.3166 21.0976 14.6834 21.0976 14.2929 20.7071L6.29289 12.7071C6.10536 12.5196 6 12.2652 6 12C6 11.7348 6.10536 11.4804 6.29289 11.2929L14.2929 3.29289C14.6834 2.90237 15.3166 2.90237 15.7071 3.29289Z" fill="currentColor" />
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M8.41436 12.0001L15.7073 4.70718L14.293 3.29297L5.58594 12.0001L14.293 20.7072L15.7073 19.293L8.41436 12.0001Z" fill="currentColor" />
         </svg>
       </Link>
     </div>
