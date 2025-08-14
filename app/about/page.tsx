69c87614270b350024f107487db2b56c4fe0d997
kyyamaguchi <kyyamaguchi@dons.usfca.edu>
2025-08-14 14:46:07 -0700

    Updated Headshot and bio

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,15 +10,12 @@ export default function About() {
       <Spacer size={64} />
       <article className={styles.about}>
         <Image
-          src="/headshot.png"
+          src="/HEADSHOT_FINAL2.jpg"
           alt="A headshot of Kala Yamaguchi"
           width={310}
           height={417}
         />
         <section className={styles.text}>
-          <p>
-            <strong>Hey, I&apos;m Kala.</strong>
-          </p>
           <p>
             I am a graphic designer currently finishing my undergraduate degree at the University of San Francisco. As a designer, I have a deep appreciation and admiration for the complexities and intricacies of the design process. For me, the creative process is a journey and an adventure. The final output is the culmination of this exploration. My work spans a number of disciplines, ranging from brand identity design, publication design, and some illustration. Currently, I have been given the honor of working as lead in-house designer at the University of San Francisco within the Career Services department. In this role, I lead a small team in creating visual identities for career-related conferences and events, as well as designing essential collateral materials that support the office&apos;s initiatives.
           </p>
